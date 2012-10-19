833b116b4d59a3937ac3b7bab25d93b3cca09da2
soywiz <soywiz@gmail.com>
2012-10-19 15:32:39 +0200

    - Small deploy improvement

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd %~dp0
 del deploy.zip 2> NUL
-"%~dp0\tools\7z" a deploy.zip -r -xr!.git -xr!obj -xr!*.pdb -xr!all_objs
+"%~dp0\tools\7z" a -tzip -mx=9 deploy.zip -r -x!.git -x!obj -x!project/obj -xr!*.pdb -xr!all_objs -x!tools -x!deploy.bat
 haxelib submit deploy.zip
 del deploy.zip 2> NUL
 popd
